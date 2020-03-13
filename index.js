5168f6b03503a079f82fdfbf0e495adb3b95c033
grumpyCello <kfoniciello@gmail.com>
2020-03-13 12:58:42 -0600

    got the homepage to include all the popup/slide-up .ejs pages

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fs = require('fs')
 const ejs = require('ejs')
 const path = require('path')
 let app = express()
@@ -10,7 +11,25 @@ app.set('port', process.env.PORT || 5000)
    .use(express.static(path.join(__dirname, 'public')))
 
 Router.get('/', (req, res) => {
-    res.render(app.get('views') + '/pages/homepage.ejs')
+    let addNewCar   = fs.readFileSync(app.get('views') + '/pages/addNewCar_popup.ejs', 'utf8')
+    let carsTracked = fs.readFileSync(app.get('views') + '/pages/carsTrackedSlide.ejs', 'utf8')
+    let gas         = fs.readFileSync(app.get('views') + '/pages/gas_slide-up.ejs', 'utf8')
+    let maintenance = fs.readFileSync(app.get('views') + '/pages/maintenance_slide-up.ejs', 'utf8')
+    let oil         = fs.readFileSync(app.get('views') + '/pages/oil_slide-up.ejs', 'utf8')
+    let signInUp    = fs.readFileSync(app.get('views') + '/pages/signInUp-popup.ejs', 'utf8')
+    let homepage    = fs.readFileSync(app.get('views') + '/pages/homepage.ejs', 'utf8')
+    let data = {
+        addNewCar: addNewCar,
+        carsTracked: carsTracked,
+        gas: gas,
+        maintenance: maintenance,
+        oil: oil,
+        signInUp: signInUp
+    }
+    mainpage = ejs.render(homepage, data)
+
+    res.setHeader('Content-Type', 'text/html');
+    res.send(mainpage);
 })
 
 app.use('/', Router)
