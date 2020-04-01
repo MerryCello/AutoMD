034fca6f43d4ff118bcf14ecd110aaf15db8623a
GrumpyCello <kfoniciello@gmail.com>
2020-03-31 22:30:36 -0600

    Added the popup for adding cars and the slides for the bottom nav btns. No animation at this point but they appear and go away generaly fine.

--- public/js/amdFunctions.js
+++ public/js/amdFunctions.js
@@ -1,3 +1,5 @@
+// const e = require("express");
+
 document.addEventListener("DOMContentLoaded", function() {
     // Display page last modified date
     lastModified();
@@ -17,3 +19,35 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("sideMenu").style.display = "initial";
 }
+
+/*
+ * Popups
+ */
+let openPopup = (event) => {
+    // $('#' + popupId).toggleClass('show');
+    event.preventDefault();
+    $(".popup").fadeIn(300, () => { $(this).focus(); });
+}
+let closePopups = () => {
+    $('.popup').fadeOut(300);
+}
+
+/*
+ * Slides
+ */
+let openSlide = (event) => {
+    event.preventDefault();
+    $('.' + event.target.id).fadeIn(300, () => { $(this).focus(); });
+}
+let closeSlides = (event) => {
+    $('.slide').fadeOut(300);
+}
+
+$(document).ready(() => {
+    $('.popupLink').click(openPopup);
+    $('.close').click(closePopups);
+    $('.popup').on('blur', closePopups);
+    $('.slideLink').click(openSlide);
+    $('.close').click(closeSlides);
+    $('.slide').on('blur', closeSlides);
+})
